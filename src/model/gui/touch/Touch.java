7f0215d9ee9ccb8dad83edb9ed741390c30e2bfb
efwright <efwright@udel.edu>
2016-11-06 22:26:49 -0500

    Add files via upload

---
+++ src/model/gui/touch/Touch.java
@@ -0,0 +1,76 @@
+package model.gui.touch;
+
+import java.awt.Graphics;
+import model.drawing.DrawableObject;
+
+/**
+ * Touch
+ * Touch keeps a pointer to a DrawableObject
+ * Touch is exclusively used for dragging DrawableObjects around the screen
+ * Touch needs to be explicitly told when to pick something up and when to drop something using clamp and unClamp
+ * The Touch pointer should never be null, because Touch should be able to draw the object it's holding
+ * 
+ * @author Eric
+ *
+ */
+
+public class Touch {
+	
+	private DrawableObject holding;
+	private DrawableObject nullObject;
+	private boolean isHolding;
+	
+	public Touch(){
+		nullObject = new DefaultTouchObject();
+		holding = nullObject;
+		isHolding = false;
+	}
+	
+	public void mouseDragged(int mouseX, int mouseY){
+		this.holding.setCoord(mouseX, mouseY);
+	}
+	
+	public void clamp(DrawableObject objectToBeClamped){
+		this.holding = objectToBeClamped;
+	}
+	
+	public void unClamp(){
+		holding = nullObject;
+	}
+	
+	public void draw(Graphics g){
+		holding.draw(g);
+	}
+	
+	public DrawableObject getHolding() {
+		return holding;
+	}
+
+	public void setHolding(DrawableObject holding) {
+		this.holding = holding;
+	}
+
+	public DrawableObject getNullObject() {
+		return nullObject;
+	}
+
+	public void setNullObject(DrawableObject nullObject) {
+		this.nullObject = nullObject;
+	}
+
+	public boolean isHolding() {
+		return isHolding;
+	}
+
+	public void setHolding(boolean isHolding) {
+		this.isHolding = isHolding;
+	}
+
+	public String toString(){
+		String str = "";
+		str += "Touch is holding...\n";
+		str += holding.toString();
+		return str;
+	}
+
+}
