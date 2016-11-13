6256ddfba534b7399ce66e1057250a60208e82b6
roychengyl <roycheng@udel.edu>
2016-11-13 15:58:06 -0500

    Update Pollutant.java

--- src/model/grid/griditem/trailitem/Pollutant.java
+++ src/model/grid/griditem/trailitem/Pollutant.java
@@ -1,5 +1,33 @@
 package model.grid.griditem.trailitem;
 
+import java.util.Collection;
+
+import com.sun.scenario.effect.Offset;
+
+import model.*;
+import model.drawing.Coord;
+import model.grid.GridColor;
+import model.grid.gridcell.GridPosition;
+import model.moving.Velocity;
+
+/**
+ * 
+ * Pollutant are stuff that's bad for the estuary, you are supposed to pick them up.
+ * 
+ * @author Roy Cheng
+ * @version 1
+ * @attributes health - health of the trailitems
+ * @attributes speed - how fast they travel in the river
+ * @attributes isBad - if it is bad to click on it
+ * 
+ */
+
 public class Pollutant extends TrailItem{
+
+	public Pollutant(Coord coord, model.drawing.Animation animation, GridPosition gridPosition, Velocity velocity) {
+		super(coord, animation, gridPosition, velocity);
+		gridcolor = GridColor.RED;
+		animation = new Animation("pollutant", Offset.CENTER, Offset.CENTER);
+	}
 	
 }
