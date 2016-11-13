a6cb95a310451c950189928bbe4e986c94d08309
roychengyl <roycheng@udel.edu>
2016-11-13 15:08:04 -0500

    Update Grid.java

--- src/model/grid/Grid.java
+++ src/model/grid/Grid.java
@@ -1,7 +1,74 @@
 package model.grid;
 
-import model.grid.gridcell.GridCell;
+import java.util.Collection;
 
-public class Grid extends Component {
+import model.*;
+
+import model.grid.gridcell.GridCell;
+import model.grid.griditem.GridItem;
+import model.grid.griditem.trailitem.TrailItem;
+import model.gui.touch.Touch;
+
+
+///////////////HAS LOTS OF ERRORS, WILL DEBUG AFTER ALL CLASSES ARE IMPLEMENTED/////////////
+public class Grid {
 	
+	private Collection<GridItem> items;
+	private Collection<Tower> towers;
+	private Collection<TrailItem> trailItems;
+	private Collection<Gabion> gabions;
+	private Collection<Path> paths;
+	private Difficulty difficulty;
+	private Touch touch;
+	
+	static private Grid grid = null;
+	//// Attributes ////
+	
+	static public Grid getInstance(){
+		if(grid == null){
+			grid = new Grid();
+		}
+		return grid;
+	}
+	
+	//// Constructor ////
+	public Grid(){
+		Dimension d = Toolkit.getDefaultToolkit().getScreenSize();
+		cells = new DirectionGrid();
+		System.out.println(cells);
+		items = new ArrayList<GridItem>();
+		towers = new ArrayList<Tower>();
+		trailItems = new ArrayList<TrailItem>();
+		gabions = new ArrayList<Gabion>();
+		paths = new ArrayList<Path>();
+		difficulty = new Difficulty();
+		new ImageLibrary();
+		touch = new Touch();
+		towerComponent = new Structures((int) d.getWidth(), (int) d.getHeight(), pixelGrid);
+	}
+	
+	//// Getters and Setters ////
+	public Structures getTowerComponent(){
+		return towerComponent;
+	}
+	public DirectionGrid getCells(){
+		return cells;
+	}
+	public Collection<GridItem> getItems(){
+		return items;
+	}
+	public Collection<Tower> getTowers(){
+		return towers;
+	}
+	public Collection<TrailItem> getTrailItems(){
+		return trailItems;
+	}
+	public Collection<Gabion> getGabions(){
+		return gabions;
+	}
+	public int getGridSize(){
+		return cells.getGridSize();
+	}
+	
+
 }
