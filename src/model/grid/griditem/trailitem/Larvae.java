2ddaf9510c6325b3fad7a398fedf176ddf4a5a80
roychengyl <roycheng@udel.edu>
2016-11-13 16:14:50 -0500

    Update Larvae.java

--- src/model/grid/griditem/trailitem/Larvae.java
+++ src/model/grid/griditem/trailitem/Larvae.java
@@ -1,5 +1,35 @@
 package model.grid.griditem.trailitem;
 
+import java.util.Collection;
+
+import com.sun.scenario.effect.Offset;
+
+import model.*;
+import model.drawing.Animation;
+import model.drawing.Coord;
+import model.grid.GridColor;
+import model.grid.gridcell.GridPosition;
+import model.moving.Velocity;
+
+/**
+ * 
+ * Larvae are stuff that can be picked up anywhere, but you aren't supposed to pick any of them up,
+ * since it is suppose to be in the estuary
+ * 
+ * @author Roy Cheng
+ * @version 1
+ * @attributes health - health of the trailitems
+ * @attributes speed - how fast they travel in the river
+ * @attributes isBad - if it is bad to click on it
+ * 
+ */
+
 public class Larvae extends TrailItem{
-	
+
+	public Larvae(Coord coord, Animation animation, GridPosition gridPosition, Velocity velocity) {
+		super(coord, animation, gridPosition, velocity);
+		gridcolor = GridColor.YELLOW;
+		animation = new Animation("larvae", Offset.CENTER, Offset.CENTER);
+	}
+
 }
