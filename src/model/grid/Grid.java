f00ac7b99d2a6041699b79e4f58148f3751ced87
efwright <efwright@udel.edu>
2016-11-13 14:47:01 -0500

    Update Grid.java

--- src/model/grid/Grid.java
+++ src/model/grid/Grid.java
@@ -4,6 +4,21 @@ import model.grid.gridcell.GridCell;
 
 public class Grid extends Component {
 	
+	private Grid grid = null;
 	
+	public Grid(){
+	}
+	
+	public void initialize(){
+		this.grid = new Grid();
+	}
+	
+	public static Grid getInstance(){
+		return this.grid;
+	}
+	
+	public Acceleration getAcceleration(GridPosition gridPosition){
+		return null;	
+	}
 
 }
