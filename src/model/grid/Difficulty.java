9e0d3467c63f07b02b81a46a88ef25e2223c8cf6
sstanitski <sstanitski@gmail.com>
2016-11-14 14:16:00 -0500

    Update Difficulty.java

--- src/model/grid/Difficulty.java
+++ src/model/grid/Difficulty.java
@@ -1,11 +1,145 @@
 package model.grid;
 
-/**
- * 
- * @author eric
- *
- */
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.ThreadLocalRandom;
+
+import model.*;
+import model.grid.Grid;
+import model.grid.gridcell.GridPosition;
+import model.grid.griditem.trailitem.Oyster;
+import model.grid.griditem.trailitem.TrailItem;
 
 public class Difficulty {
+	
+	private int timeToSpawn;
+	private final int SPAWN_TIME = 120;
+	private int oysterCount;
+	private int invasiveCount;
+	private int pollutantCount;
+	private int larvaeCount;
+	
+	public Difficulty(){
+		timeToSpawn = SPAWN_TIME;
+	}
+	
+	/**
+	 * Returns void
+	 * <p>
+	 * Look at the current Grid and decide how to scale difficulty and when to spawn new objects
+	 */
+	public void update(){
+		if(timeToSpawn > 0){
+			timeToSpawn--;
+		} else {
+			spawn();
+			timeToSpawn = SPAWN_TIME;
+		}
+	}
+	
+	/**
+	 * Place the trail item into the Grid
+	 * 
+	 */
+	
+	//this needs to be fixed
+	public void spawn(){
+		TrailItem toBeSpawned = decideSpawn();
+		List<GridPosition> listOfSpawnPoints = Grid.getInstance().getCells().getSpawnPositions();
+		int randomIndex = ThreadLocalRandom.current().nextInt(0, listOfSpawnPoints.size());
+		GridPosition spawnPoint = listOfSpawnPoints.get(randomIndex);
+		toBeSpawned.setPosn(spawnPoint);
+		GridPosition pixelPosn = Grid.getInstance().getPixelGrid().getValidPixelPosn(spawnPoint);
+		toBeSpawned.setPixelPosn(pixelPosn);
+		toBeSpawned.setPixelPosn(new Posn(toBeSpawned.getPixelPosn().getX(), 0));
+		Grid.getInstance().addPath(new Path(toBeSpawned, pixelPosn, PathBehavior.STRAIGHT, PathTermination.TO_GRID, 1));
+	}
+	
+	/**
+	 * Returns void
+	 * <p>
+	 * Use the difficulty and what has been spawned recently to decide
+	 * what needs to be spawned, avoid unwinnable scenarios
+	 */
+	public TrailItem decideSpawn(){
+		//(Poissen process? maybe not) in time the random should have a nice average between the 4
+		//if by chance it doesnt, the spawn rate should be adjusted when the item is called by our random.
+		int randomNum = (int)(Math.random() * 3);
+		
+		if(randomNum == 0){
+			oysterCount++;
+			if (timeToSpawn < 90 && oysterCount < 4){
+				return new Oyster();//need to change speed(slower) or spawn rate(more)
+			}
+			else if(timeToSpawn < 90 && oysterCount > 6){
+				return new Oyster();//need to change speed(faster) or spawn rate(less)
+			}
+			if (timeToSpawn < 30 && oysterCount < 12){
+				return new Oyster();//need to change speed(slower) or spawn rate(more)
+			}
+			else if (timeToSpawn < 30 && oysterCount > 16){
+				return new Oyster();//need to change speed(faster) or spawn rate(less)
+			}
+			return new Oyster();
+		}
+		else if (randomNum == 1){
+			invasiveCount++;
+			if (timeToSpawn < 90 && invasiveCount < 4){
+				return new InvasiveItem();//need to change speed(slower) or spawn rate(more)
+			}
+			else if(timeToSpawn < 90 && invasiveCount > 6){
+				return new InvasiveItem();//need to change speed(faster) or spawn rate(less)
+			}
+			if (timeToSpawn < 30 && invasiveCount < 12){
+				return new InvasiveItem();//need to change speed(slower) or spawn rate(more)
+			}
+			else if (timeToSpawn < 30 && invasiveCount > 16){
+				return new InvasiveItem();//need to change speed(faster) or spawn rate(less)
+			}
+			return new InvasiveItem();
+		}
+		else if (randomNum == 2){
+			larvaeCount++;
+			if (timeToSpawn < 90 && larvaeCount < 4){
+				return new Oyster();//need to change speed(slower) or spawn rate(more)
+			}
+			else if(timeToSpawn < 90 && larvaeCount > 6){
+				return new Oyster();//need to change speed(faster) or spawn rate(less)
+			}
+			if (timeToSpawn < 30 && larvaeCount < 12){
+				return new Oyster();//need to change speed(slower) or spawn rate(more)
+			}
+			else if (timeToSpawn < 30 && larvaeCount > 16){
+				return new Oyster();//need to change speed(faster) or spawn rate(less)
+			}
+			return new Larvae();
+		}
+		else{	
+			//if we are still giving pollutants "health" or "drags" then we need to make it known in pollutant constructor
+			pollutantCount++;
+			int randomPol = (int)(Math.random() * 2); 
+			if (timeToSpawn < 90 && pollutantCount < 4){
+				return new Pollutant(1);//need to change speed(slower) or spawn rate(more)
+			}
+			else if(timeToSpawn < 90 && pollutantCount > 6){
+				return new Pollutant(3);//need to change speed(faster) or spawn rate(less)
+			}
+			if (timeToSpawn < 30 && pollutantCount < 12){
+				return new Pollutant(1);//need to change speed(slower) or spawn rate(more)
+			}
+			else if (timeToSpawn < 30 && pollutantCount > 16){
+				return new Pollutant(3);//need to change speed(faster) or spawn rate(less)
+			}
+			if(randomPol == 0){
+				return new Pollutant(1);
+			}
+			else if (randomPol == 1){
+				return new Pollutant(2);
+			}
+			else (randomPol == 2){
+				return new Pollutant(3);
+			}
+		}
+	}
 
 }
