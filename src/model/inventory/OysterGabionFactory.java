98b80708c62450f8914ce83490e58a28019d4f04
efwright <efwright@udel.edu>
2016-11-13 21:06:22 -0500

    tower factories

---
+++ src/model/inventory/OysterGabionFactory.java
@@ -0,0 +1,41 @@
+package model.inventory;
+	
+import model.gui.component.ComponentPosition;
+
+/**
+ * 
+ * @author eric
+ *
+ */
+
+public class OysterGabionFactory extends TowerFactory {
+	
+	private int oysters;
+	private final int OYSTERS_PER_GABION = 4;
+
+	public OysterGabionFactory(ComponentPosition topLeft, int width, int height) {
+		super(topLeft, width, height);
+		oysters = 0;
+	}
+	
+	public OysterGabionFactory(int x, int y, int width, int height){
+		super(x, y, width, height);
+		oysters = 2;
+	}
+	
+	@Override
+	public void mouseClicked(int mouseX, int mouseY){
+		// If applicable, clamp new tower to touch
+		if(oysters >= OYSTERS_PER_GABION){
+			oysters -= OYSTERS_PER_GABION;
+			Touch.clamp(new OysterGabion(new Coord(topLeft.getX(), topLeft.getY())));
+		}
+	}
+	
+	@Override
+	public void mouseReleased(int mouseX, int mouseY){
+		// Do nothing
+		Touch.snap();
+	}
+
+}
