447bd4f1712f7fec1a4a53b6f277490b13870c69
efwright <efwright@udel.edu>
2016-11-13 22:59:37 -0500

    fixes to grid items

---
+++ src/model/grid/griditem/griditem/trailitem/Larvae.java
@@ -0,0 +1,34 @@
+package model.grid.griditem.trailitem;
+
+import java.util.Collection;
+
+import model.*;
+import model.drawing.Animation;
+import model.drawing.Coord;
+import model.drawing.Offset;
+import model.grid.gridcell.GridPosition;
+import model.grid.griditem.GridColor;
+import model.moving.Velocity;
+
+/**
+ * 
+ * Larvae are stuff that can be picked up anywhere, but you aren't supposed to pick any of them up,
+ * since it is suppose to be in the estuary
+ * 
+ * @author Roy Cheng, Eric
+ * @version 1
+ * @attributes health - health of the trailitems
+ * @attributes speed - how fast they travel in the river
+ * @attributes isBad - if it is bad to click on it
+ * 
+ */
+
+public class Larvae extends TrailItem{
+
+	public Larvae(Coord coord, Animation animation, GridPosition gridPosition, Velocity velocity) {
+		super(coord, animation, gridPosition, velocity);
+		setGridColor(GridColor.WHITE);
+		animation = new Animation("larvae", Offset.CENTER, Offset.CENTER);
+	}
+
+}
---
+++ src/model/grid/griditem/griditem/trailitem/Pollutant.java
@@ -0,0 +1,33 @@
+package model.grid.griditem.trailitem;
+
+import java.util.Collection;
+
+import model.*;
+import model.drawing.*;
+import model.drawing.Animation;
+import model.drawing.Coord;
+import model.grid.gridcell.GridPosition;
+import model.grid.griditem.GridColor;
+import model.moving.Velocity;
+
+/**
+ * 
+ * Pollutant are stuff that's bad for the estuary, you are supposed to pick them up.
+ * 
+ * @author Roy Cheng, Eric
+ * @version 1
+ * @attributes health - health of the trailitems
+ * @attributes speed - how fast they travel in the river
+ * @attributes isBad - if it is bad to click on it
+ * 
+ */
+
+public class Pollutant extends TrailItem{
+
+	public Pollutant(Coord coord, Animation animation, GridPosition gridPosition, Velocity velocity) {
+		super(coord, animation, gridPosition, velocity);
+		setGridColor(GridColor.RED);
+		animation = new Animation("pollutant", Offset.CENTER, Offset.CENTER);
+	}
+	
+}
