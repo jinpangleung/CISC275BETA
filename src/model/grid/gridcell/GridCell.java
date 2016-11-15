253c487063106b621b298eb246369593456192f7
sstanitski <sstanitski@gmail.com>
2016-11-14 19:50:26 -0500

    Update GridCell.java

--- src/model/grid/gridcell/GridCell.java
+++ src/model/grid/gridcell/GridCell.java
@@ -1,17 +1,50 @@
 package model.grid.gridcell;
 
-/**
- * GridCell
- * A spot on the underlying Grid of the game
- * Mostly controls direction of the water flowing
- * 
- * @author Eric
- *
- */
-
 public class GridCell {
 	
 	private GridPosition gridPosition;
+	public boolean isTrail;
+	public boolean isLand;
+	public Direction direction;
+	
+	public GridCell(GridPosition g, boolean isT, boolean isL, Direction d){
+		this.gridPosition = g;
+		this.isTrail = isT;
+		this.isLand = isL;
+		this.direction = d;
+	}
+
+	public Direction getDirection() {
+		return direction;
+	}
+
+	public void setDirection(Direction direction) {
+		this.direction = direction;
+	}
+
+	public GridPosition getGridPosition() {
+		return gridPosition;
+	}
+
+	public void setGridPosition(GridPosition gridPosition) {
+		this.gridPosition = gridPosition;
+	}
+
+	public boolean isTrail() {
+		return isTrail;
+	}
+
+	public void setTrail(boolean isTrail) {
+		this.isTrail = isTrail;
+	}
+
+	public boolean isLand() {
+		return isLand;
+	}
+
+	public void setLand(boolean isLand) {
+		this.isLand = isLand;
+	}
 	
 
 }
