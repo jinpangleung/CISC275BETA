9a9c2e140765ca10a38a15f44ad0acd7ff473b02
jinpangleung <leung@udel.edu>
2016-11-14 20:16:24 -0500

    Update ConcreteGabion.java

--- src/model/grid/griditem/gabion/ConcreteGabion.java
+++ src/model/grid/griditem/gabion/ConcreteGabion.java
@@ -3,33 +3,10 @@ package model.grid.griditem.gabion;
 import model.drawing.Animation;
 import model.drawing.Coord;
 import model.grid.gridcell.GridPosition;
-import model.grid.griditem.GridItem;
 
-/**
- * 
- * @author leung
- *
- */
+public class ConcreteGabion extends Gabion {
 
-public class Gabion extends GridItem {
-	
-	int health = 100;
-
-	public Gabion(Coord coord, Animation animation, GridPosition gridPosition) {
+	public ConcreteGabion(Coord coord, Animation animation, GridPosition gridPosition){
 		super(coord, animation, gridPosition);
-		// TODO Auto-generated constructor stub
 	}
-	
-	public int getHealth(){
-		return health;
-	}
-	
-	public void takeDamage(){
-		health = 0;
-	}
-	
-	public static void snap(GridItem gridI, Coord dest){
-		gridI.setCoord(dest);
-	}
-
 }
