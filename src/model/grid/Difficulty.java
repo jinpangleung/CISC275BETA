73b34eac374b69def1c06e06473c6522cdf4cf81
jinpangleung <leung@udel.edu>
2016-11-15 18:13:00 -0500

    Update Difficulty.java

--- src/model/grid/Difficulty.java
+++ src/model/grid/Difficulty.java
@@ -5,10 +5,16 @@ import java.util.List;
 import java.util.concurrent.ThreadLocalRandom;
 
 import model.*;
+import model.drawing.Coord;
 import model.grid.Grid;
+import model.grid.gridcell.Direction;
 import model.grid.gridcell.GridPosition;
+import model.grid.griditem.trailitem.InvasiveItem;
+import model.grid.griditem.trailitem.Larvae;
 import model.grid.griditem.trailitem.Oyster;
+import model.grid.griditem.trailitem.Pollutant;
 import model.grid.griditem.trailitem.TrailItem;
+import model.gui.path.*;
 
 public class Difficulty {
 	
@@ -48,11 +54,11 @@ public class Difficulty {
 		List<GridPosition> listOfSpawnPoints = Grid.getInstance().getCells().getSpawnPositions();
 		int randomIndex = ThreadLocalRandom.current().nextInt(0, listOfSpawnPoints.size());
 		GridPosition spawnPoint = listOfSpawnPoints.get(randomIndex);
-		toBeSpawned.setPosn(spawnPoint);
-		GridPosition pixelPosn = Grid.getInstance().getPixelGrid().getValidPixelPosn(spawnPoint);
-		toBeSpawned.setPixelPosn(pixelPosn);
-		toBeSpawned.setPixelPosn(new Posn(toBeSpawned.getPixelPosn().getX(), 0));
-		Grid.getInstance().addPath(new Path(toBeSpawned, pixelPosn, PathBehavior.STRAIGHT, PathTermination.TO_GRID, 1));
+		toBeSpawned.setGridPosition(spawnPoint);
+		GridPosition coord = Grid.getInstance().getPixelGrid().getValidPixelPosn(spawnPoint);
+		toBeSpawned.setCoord(coord.getX(), 0);
+//		toBeSpawned.setCoord(new Coord(toBeSpawned.getCoord().getX(), 0));
+		Grid.getInstance().addPath(new Path(toBeSpawned, coord, Direction.STRAIGHT, PathTermination.TO_GRID, 1));
 	}
 	
 	/**
@@ -101,44 +107,45 @@ public class Difficulty {
 		else if (randomNum == 2){
 			larvaeCount++;
 			if (timeToSpawn < 90 && larvaeCount < 4){
-				return new Oyster();//need to change speed(slower) or spawn rate
+				return new Larvae();//need to change speed(slower) or spawn rate
 			}
 			else if(timeToSpawn < 90 && larvaeCount > 6){
-				return new Oyster();//need to change speed(faster) or spawn rate
+				return new Larvae();//need to change speed(faster) or spawn rate
 			}
 			if (timeToSpawn < 30 && larvaeCount < 12){
-				return new Oyster();//need to change speed(slower) or spawn rate
+				return new Larvae();//need to change speed(slower) or spawn rate
 			}
 			else if (timeToSpawn < 30 && larvaeCount > 16){
-				return new Oyster();//need to change speed(faster) or spawn rate
+				return new Larvae();//need to change speed(faster) or spawn rate
 			}
 			return new Larvae();
 		}
 		else{	
 			//if we are still giving pollutants "health" or "drags" then we need to make it known in pollutant constructor
 			pollutantCount++;
-			int randomPol = (int)(Math.random() * 2); 
+//			int randomPol = (int)(Math.random() * 2); 
 			if (timeToSpawn < 90 && pollutantCount < 4){
-				return new Pollutant(1);//need to change speed(slower) or spawn rate
+				return new Pollutant();//need to change speed(slower) or spawn rate
 			}
 			else if(timeToSpawn < 90 && pollutantCount > 6){
-				return new Pollutant(3);//need to change speed(faster) or spawn rate
+				return new Pollutant();//need to change speed(faster) or spawn rate
 			}
 			if (timeToSpawn < 30 && pollutantCount < 12){
-				return new Pollutant(1);//need to change speed(slower) or spawn rate
+				return new Pollutant();//need to change speed(slower) or spawn rate
 			}
 			else if (timeToSpawn < 30 && pollutantCount > 16){
-				return new Pollutant(3);//need to change speed(faster) or spawn rate
+				return new Pollutant();//need to change speed(faster) or spawn rate
 			}
-			if(randomPol == 0){
-				return new Pollutant(1);
-			}
-			else if (randomPol == 1){
-				return new Pollutant(2);
-			}
-			else (randomPol == 2){
-				return new Pollutant(3);
-			}
+//			if(randomPol == 0){
+//				return new Pollutant(1);
+//			}
+//			else if (randomPol == 1){
+//				return new Pollutant(2);
+//			}
+//			else if (randomPol == 2){
+//				return new Pollutant(3);
+//			}
+		return new Pollutant();
 		}
 	}
 
