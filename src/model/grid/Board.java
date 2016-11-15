203ab2548604f3e744599fd7257fd8c08ae562df
sstanitski <sstanitski@gmail.com>
2016-11-14 21:26:47 -0500

    Update Board.java

--- src/model/grid/Board.java
+++ src/model/grid/Board.java
@@ -1,1 +1,39 @@
 
+package model.grid;
+
+import model.grid.gridcell.GridCell;
+
+public class Board {
+	
+	private GridCell[][] gridCell;
+	
+	// Constructor
+	public Board(){
+		try{
+			// Read from grid.txt
+			Path path = Paths.get("grid.txt");
+		    List<String> lines = Files.readAllLines(path, StandardCharsets.UTF_8);
+		    gridSize = lines.get(0).length();
+		    System.out.println("Grid Size " + Integer.toString(gridSize));
+		} catch (IOException e){
+			System.out.println("Could not open grid.txt");
+			throw new RuntimeException();
+		}
+	}
+	
+	public void getSquareWidth(){
+		
+	}
+	public void getSquareHeight(){
+		
+	}
+	public void getSquareSize(){
+		
+	}
+	public void getGridCell(int x, int y){
+		
+	}
+	public class DirectionGrid {
+		
+	}
+}
