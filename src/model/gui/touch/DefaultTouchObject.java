b63db89c8682ec8aa17c777061e1e4f9d680fa14
efwright <efwright@udel.edu>
2016-11-14 20:51:17 -0500

    Update DefaultTouchObject.java

--- src/model/gui/touch/DefaultTouchObject.java
+++ src/model/gui/touch/DefaultTouchObject.java
@@ -3,11 +3,13 @@ package model.gui.touch;
 import model.drawing.Animation;
 import model.drawing.Coord;
 import model.drawing.DrawableObject;
+import model.grid.gridcell.GridPosition;
+import model.grid.griditem.GridItem;
 
-public class DefaultTouchObject extends DrawableObject {
+public class DefaultTouchObject extends GridItem {
 
 	public DefaultTouchObject() {
-		super(new Coord(0.0, 0.0), new Animation());
+		super(new Coord(0.0, 0.0), new Animation(), new GridPosition(0, 0));
 	}
 
 }
