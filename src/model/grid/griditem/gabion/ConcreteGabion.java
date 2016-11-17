2cfbc1cf122cbe5212eb279414fc898636cb4dbd
roychengyl <roycheng@udel.edu>
2016-11-16 23:48:22 -0500

    Update ConcreteGabion.java

--- src/model/grid/griditem/gabion/ConcreteGabion.java
+++ src/model/grid/griditem/gabion/ConcreteGabion.java
@@ -4,6 +4,16 @@ import model.drawing.Animation;
 import model.drawing.Coord;
 import model.grid.gridcell.GridPosition;
 
+
+/**
+ * ConcretGabion
+ * a gabion that has limited amount of it, where it could help to stop damage from storm
+ * 
+ * @author Roy
+ *
+ */
+
+
 public class ConcreteGabion extends Gabion {
 
 	public ConcreteGabion(Coord coord, Animation animation, GridPosition gridPosition){
