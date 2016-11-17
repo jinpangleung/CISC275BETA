2bfd8dfc2a7651c1ec74da356406fcac38a9d132
jinpangleung <leung@udel.edu>
2016-11-16 20:16:20 -0500

    Update ConcreteGabionFactory.java

--- src/model/inventory/factory/ConcreteGabionFactory.java
+++ src/model/inventory/factory/ConcreteGabionFactory.java
@@ -37,7 +37,7 @@ public class ConcreteGabionFactory extends TowerFactory {
 	@Override
 	public void mouseReleased(int mouseX, int mouseY){
 		// Do nothing
-		Touch.snap();
+		Path.snap();
 	}
 
 }
